d0b946c4f5ebc5518ad301bd22498e513d7685ae
yuanlee <yuanlee@163.com>
2024-04-06 00:37:57 +0800

    chore: Vite 预览代理到 json-server

--- vite.config.ts
+++ vite.config.ts
@@ -17,10 +17,11 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
     VITE_PORT,
     VITE_INTERNAL_VERSION,
     VITE_APP_TITLE,
-    VITE_PERMISSION,
     VITE_BASE_API,
     VITE_PROXY,
+    VITE_PREVIEW_PROXY
   } = env;
+
   const __APP_VERSION__ = [pkg.version, VITE_INTERNAL_VERSION].join(".");
   const __APP_BUILD_TIME__ = new Date().toISOString();
   const __APP_INFO__ = {
@@ -53,23 +54,35 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
         "@": fileURLToPath(new URL("./src", import.meta.url)),
       },
     },
-    server: {
-      port: Number(VITE_PORT),
-      proxy:
-        VITE_PERMISSION && JSON.parse(VITE_PERMISSION)
-          ? {
-              [VITE_BASE_API]: {
-                target: VITE_PROXY,
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api\//, "/api/v1/"),
-              },
-            }
-          : undefined,
-    },
     define: {
       __APP_VERSION__: JSON.stringify(__APP_VERSION__),
       __APP_BUILD_TIME__: JSON.stringify(__APP_BUILD_TIME__),
       __APP_INFO__: JSON.stringify(__APP_INFO__),
+    },
+    server: {
+      port: Number(VITE_PORT),
+      proxy:
+          VITE_PROXY
+          ? {
+              [VITE_BASE_API]: {
+                target: VITE_PROXY,
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api\//, ""),
+              },
+            }
+          : void 0,
+    },
+    preview: {
+      proxy:
+          VITE_PREVIEW_PROXY
+          ? {
+              [VITE_BASE_API]: {
+                target: VITE_PREVIEW_PROXY,
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api\//, ""),
+              },
+            }
+          : void 0,
     },
   };
 });
