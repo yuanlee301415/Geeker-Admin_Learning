46769ae7dc7d0dbdd84e3abe020021c45b566a3e
yuanlee <yuanlee@163.com>
2024-04-11 21:13:56 +0800

    feat: 搜索菜单

--- src/layouts/compoents/Header/components/SearchMenu.vue
+++ src/layouts/compoents/Header/components/SearchMenu.vue
@@ -3,21 +3,23 @@
 -->
 <template>
   <div class="header-toolbar_search">
-    <i class="iconfont icon-sousuo" @click="visible = true" />
+    <i class="iconfont icon-sousuo" @click="handleOpen" />
     <el-dialog
       v-model="visible"
       :modal="false"
       :show-close="false"
       fullscreen
       destroy-on-close
-      @click="visible = false"
+      @click="handleClose"
     >
       <el-autocomplete
+        ref="autocompleteRef"
         v-model="searchMenu"
-        :fetch-suggestions="menuList"
+        :fetch-suggestions="searchMenuList"
         value-key="path"
         placeholder="菜单搜索：支持菜单名称、路径"
         @click.stop
+        @select="handleSelect"
       >
         <template #prefix>
           <el-icon>
@@ -36,183 +38,53 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { Search } from '@element-plus/icons-vue'
+import { useRouter } from 'vue-router'
+import { useAuthStore } from '@/store/modules/auth'
 
+const authStore = useAuthStore()
+const router = useRouter()
 const visible = ref(false)
 const searchMenu = ref('')
-const menuList = [
-  {
-    path: '/home/index',
-    meta: {
-      icon: 'HomeFilled',
-      title: '首页'
-    }
-  },
-  {
-    path: '/proTable',
-    name: 'proTable',
-    redirect: '/proTable/useProTable',
-    meta: {
-      icon: 'MessageBox',
-      title: '超级表格',
-      isLink: '',
-      isHide: false,
-      isFull: false,
-      isAffix: false,
-      isKeepAlive: true
-    },
-    children: [
-      {
-        path: '/proTable/useProTable',
-        name: 'useProTable',
-        component: '/proTable/useProTable/index',
-        meta: {
-          icon: 'Menu',
-          title: '使用 ProTable',
-          isLink: '',
-          isHide: false,
-          isFull: false,
-          isAffix: false,
-          isKeepAlive: true
-        },
-        children: [
-          {
-            path: '/proTable/useProTable/detail/:id',
-            name: 'useProTableDetail',
-            component: '/proTable/useProTable/detail',
-            meta: {
-              icon: 'Menu',
-              title: 'ProTable 详情',
-              isLink: '',
-              isHide: true,
-              isFull: false,
-              isAffix: false,
-              isKeepAlive: true,
-              activeMenu: '/proTable/useProTable'
-            }
-          }
-        ]
-      },
-      {
-        path: '/proTable/useTreeFilter',
-        name: 'useTreeFilter',
-        component: '/proTable/useTreeFilter/index',
-        meta: {
-          icon: 'Menu',
-          title: '使用 TreeFilter',
-          isLink: '',
-          isHide: false,
-          isFull: false,
-          isAffix: false,
-          isKeepAlive: true
-        }
-      },
-      {
-        path: '/proTable/useTreeFilter/detail/:id',
-        name: 'useTreeFilterDetail',
-        component: '/proTable/useTreeFilter/detail',
-        meta: {
-          icon: 'Menu',
-          title: 'TreeFilter 详情',
-          activeMenu: '/proTable/useTreeFilter',
-          isLink: '',
-          isHide: true,
-          isFull: false,
-          isAffix: false,
-          isKeepAlive: true
-        }
-      },
-      {
-        path: '/proTable/useSelectFilter',
-        name: 'useSelectFilter',
-        component: '/proTable/useSelectFilter/index',
-        meta: {
-          icon: 'Menu',
-          title: '使用 SelectFilter',
-          isLink: '',
-          isHide: false,
-          isFull: false,
-          isAffix: false,
-          isKeepAlive: true
-        }
-      },
-      {
-        path: '/proTable/treeProTable',
-        name: 'treeProTable',
-        component: '/proTable/treeProTable/index',
-        meta: {
-          icon: 'Menu',
-          title: '树形 ProTable',
-          isLink: '',
-          isHide: false,
-          isFull: false,
-          isAffix: false,
-          isKeepAlive: true
-        }
-      },
-      {
-        path: '/proTable/complexProTable',
-        name: 'complexProTable',
-        component: '/proTable/complexProTable/index',
-        meta: {
-          icon: 'Menu',
-          title: '复杂 ProTable',
-          isLink: '',
-          isHide: false,
-          isFull: false,
-          isAffix: false,
-          isKeepAlive: true
-        }
-      },
-      {
-        path: '/proTable/document',
-        name: 'proTableDocument',
-        component: '/proTable/document/index',
-        meta: {
-          icon: 'Menu',
-          title: 'ProTable 文档',
-          isLink: 'https://juejin.cn/post/7166068828202336263/#heading-14',
-          isHide: false,
-          isFull: false,
-          isAffix: false,
-          isKeepAlive: true
-        }
-      }
-    ]
-  },
-  {
-    path: '/proTable/useProTable',
-    name: 'useProTable',
-    component: '/proTable/useProTable/index',
-    meta: {
-      icon: 'Menu',
-      title: '使用 ProTable',
-      isLink: '',
-      isHide: false,
-      isFull: false,
-      isAffix: false,
-      isKeepAlive: true
-    },
-    children: [
-      {
-        path: '/proTable/useProTable/detail/:id',
-        name: 'useProTableDetail',
-        component: '/proTable/useProTable/detail',
-        meta: {
-          icon: 'Menu',
-          title: 'ProTable 详情',
-          isLink: '',
-          isHide: true,
-          isFull: false,
-          isAffix: false,
-          isKeepAlive: true,
-          activeMenu: '/proTable/useProTable'
-        }
-      }
-    ]
+const autocompleteRef = ref()
+const menuList = computed(() => authStore.flatMenuListGet.filter((_) => !_.meta.isHide))
+
+// 打开搜索
+function handleOpen() {
+  visible.value = true
+  setTimeout(() => {
+    autocompleteRef.value.inputRef.focus()
+  })
+}
+
+// 关闭搜索
+function handleClose() {
+  visible.value = false
+  searchMenu.value = ''
+}
+
+// 搜索菜单
+function filterMethod(queryString: string) {
+  const qs = queryString.slice().trim().toLowerCase()
+  return (item: Menu.MenuOptions) =>
+    item.path.toLowerCase().includes(qs) || item.meta.title.toLowerCase().includes(qs)
+}
+
+// 搜索菜单列表
+function searchMenuList(queryString: string, cb: any) {
+  cb(queryString ? menuList.value.filter(filterMethod(queryString)) : menuList.value)
+}
+
+// 选择菜单
+function handleSelect(item: Menu.MenuOptions) {
+  if (item.meta.isLink) {
+    window.open(item.meta.isLink, '_blank')
+  } else {
+    router.push(item.path)
   }
-]
+  handleClose()
+}
 </script>
 
 <style scoped lang="less">
