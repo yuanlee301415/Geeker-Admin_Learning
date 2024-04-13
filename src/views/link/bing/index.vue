6fc2dedadf0536edc86b1c1dbf949eee5b3a2692
yuanlee <yuanlee@163.com>
2024-04-13 14:21:49 +0800

    feat: 外部链接

---
+++ src/views/link/bing/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="card content-box">
+    <iframe src="https://cn.bing.com/" width="100%" height="100%" style="border: none" />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
