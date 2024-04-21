f0af4c16371aa4d5ebef9b3d8f6b6ea9e54890db
yuanlee <yuanlee@163.com>
2024-04-21 21:07:24 +0800

    feat: 常用组件》分类选择器

---
+++ src/components/SelectFilter/index.vue
@@ -0,0 +1,101 @@
+<!--
+分类选择器
+-->
+<template>
+  <div class="select-filter w-full">
+    <dl v-for="item of data" :key="item.key" class="flex items-center py-4">
+      <dt class="pr-4">{{ item.title }}:</dt>
+      <dd>
+        <el-button
+          v-for="opt of item.options"
+          :key="opt.value"
+          :icon="opt.icon"
+          :type="
+            (
+              item.multiple
+                ? selected?.[item.key]?.includes(opt.value)
+                : selected?.[item.key] === opt.value
+            )
+              ? 'primary'
+              : ''
+          "
+          round
+          @click="handleToggle(item, opt)"
+          >{{ opt.label }}</el-button
+        >
+      </dd>
+    </dl>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+
+type Option = {
+  label: string
+  value: string
+  icon?: string
+}
+
+type Data = {
+  title: string
+  key: string
+  multiple?: boolean
+  options: Option[]
+}
+
+type ModelValue = {
+  [key: string]: any
+}
+
+const props = defineProps<{
+  modelValue?: ModelValue
+  data?: Data[]
+}>()
+
+const emit = defineEmits<{
+  'update:modelValue': [value: ModelValue]
+}>()
+
+const selected = ref<ModelValue>({})
+watch(
+  () => props.modelValue,
+  () => {
+    props.data?.forEach((item) => {
+      if (item.multiple) {
+        selected.value[item.key] = props.modelValue?.[item.key] ?? ['']
+      } else {
+        selected.value[item.key] = props.modelValue?.[item.key] ?? ''
+      }
+    })
+  },
+  {
+    deep: true,
+    immediate: true
+  }
+)
+
+// 切换选项
+function handleToggle(item: Data, opt: Option) {
+  if (item.multiple) {
+    if (opt.value === '') {
+      selected.value[item.key] = [''] // 选择了“全部”，清除其它选项
+    } else {
+      const idx = selected.value[item.key].indexOf(opt.value)
+      selected.value[item.key] = selected.value[item.key].filter((_: string) => _ !== '') // 选择了其它选项，清除“全部”选项
+      if (idx === -1) {
+        selected.value[item.key].push(opt.value) // 添加选项
+      } else {
+        selected.value[item.key].splice(idx, 1) // 删除选项
+      }
+    }
+  } else {
+    selected.value[item.key] = opt.value
+  }
+  emit('update:modelValue', selected.value)
+}
+</script>
+
+<style scoped lang="less">
+@import 'style';
+</style>
---
+++ src/views/assembly/selectFilter/index.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="card content-box assembly-box">
+    <h2>分类筛选器 🍓🍇🍈🍉</h2>
+    <SelectFilter :data="filterData" v-model="filterResult" />
+    <pre>返回值：{{ filterResult }}</pre>
+
+    <el-descriptions title="配置项 📚" :column="1" border>
+      <el-descriptions-item label="v-model">选中的值</el-descriptions-item>
+      <el-descriptions-item label="data">需要筛选的数据列表</el-descriptions-item>
+    </el-descriptions>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import SelectFilter from '@/components/SelectFilter/index.vue'
+
+const filterResult = ref({ state: '3', type: ['1', '3'] })
+const filterData = [
+  {
+    title: '物流状态(单)',
+    key: 'state',
+    options: [
+      {
+        label: '全部',
+        value: ''
+      },
+      {
+        label: '已下单',
+        value: '1',
+        icon: 'ShoppingCart'
+      },
+      {
+        label: '已发货',
+        value: '2',
+        icon: 'Van'
+      },
+      {
+        label: '已签收',
+        value: '3',
+        icon: 'Edit'
+      },
+      {
+        label: '已退回',
+        value: '4',
+        icon: 'Guide'
+      },
+      {
+        label: '已完成',
+        value: '5',
+        icon: 'CircleCheck'
+      }
+    ]
+  },
+  {
+    title: '商品类型(多)',
+    key: 'type',
+    multiple: true,
+    options: [
+      {
+        label: '全部',
+        value: ''
+      },
+      {
+        label: '食品类',
+        value: '1'
+      },
+      {
+        label: '服装类',
+        value: '2'
+      },
+      {
+        label: '家具类',
+        value: '3'
+      },
+      {
+        label: '日用品类',
+        value: '4'
+      }
+    ]
+  }
+]
+</script>
+
+<style scoped lang="less">
+@import '../style';
+</style>
