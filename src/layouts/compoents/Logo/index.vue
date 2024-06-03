2989958ac9dfb5d94fcec6651c94c53dd2a6fcfd
yuanlee <yuanlee301415@126.com>
2024-06-03 22:10:27 +0800

    feat: 点击 Logo，跳转到首页

--- src/layouts/compoents/Logo/index.vue
+++ src/layouts/compoents/Logo/index.vue
@@ -4,14 +4,18 @@ Logo
 <template>
   <div class="logo flx-center">
     <img v-if="!hiddenImg" src="~@/assets/logo.svg" alt="Geeker Admin Logo" />
-    <h1 v-if="!hiddenTxt" v-show="!collapse">
+    <h1 v-if="!hiddenTxt" v-show="!collapse" @click="router.push(HOME_URL)">
       <slot>{{ title }}</slot>
     </h1>
   </div>
 </template>
 
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
+import { HOME_URL } from '@/constants'
+
 const title = import.meta.env.VITE_APP_TITLE
+const router = useRouter()
 
 defineProps<{
   // 是否是折叠状态
@@ -32,6 +36,7 @@ defineProps<{
     width: 28px;
   }
   h1 {
+    cursor: pointer;
     margin-left: 6px;
     font-size: 21.5px;
     font-weight: bold;
